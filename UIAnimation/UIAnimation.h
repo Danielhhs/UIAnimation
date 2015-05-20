f02d9269a75aed9abb49c1603cd199636c98863c
hhuang <hhuang@microstrategy.com>
2015-05-20 11:11:40 +0800

    Initial Commit

---
+++ UIAnimation/UIAnimation.h
@@ -0,0 +1,13 @@
+//
+//  UIAnimation.h
+//  UIAnimation
+//
+//  Created by Huang Hongsen on 5/20/15.
+//  Copyright (c) 2015 cn.daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UIAnimation : NSObject
+
+@end
---
+++ UIAnimation/UIAnimation.m
@@ -0,0 +1,13 @@
+//
+//  UIAnimation.m
+//  UIAnimation
+//
+//  Created by Huang Hongsen on 5/20/15.
+//  Copyright (c) 2015 cn.daniel. All rights reserved.
+//
+
+#import "UIAnimation.h"
+
+@implementation UIAnimation
+
+@end
